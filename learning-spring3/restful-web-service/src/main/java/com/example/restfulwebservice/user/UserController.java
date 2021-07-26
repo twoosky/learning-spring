a931132ab3a5de84ba94d0bbfd7772c076450d2e
twoosky <lhjae3@naver.com>
2021-07-26 21:07:06 +0900

    Add User SErvice API

---
+++ learning-spring3/restful-web-service/src/main/java/com/example/restfulwebservice/user/UserController.java
@@ -0,0 +1,58 @@
+package com.example.restfulwebservice.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private UserDaoService service;
+
+    public UserController (UserDaoService service){
+        this.service = service;
+    }
+
+    // 전체 사용자 목록을 반환하는 메소드
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    // GET /users/1 or /users/10 ...
+    // id 값이 string type으로 Controller에 들어오는데 이를 integer로 받으면 자동으로 형태변환됨.
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = service.deleteById(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+    }
+}
