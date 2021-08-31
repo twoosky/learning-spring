110abadcff8f09db3f5c8470c8fe0e3a8fb55a43
twoosky <lhjae3@naver.com>
2021-08-31 19:16:44 +0900

    Add Project

---
+++ learning-spring4/Spring-Boot-Community-Web/src/main/java/com/web/SpringBootCommunityWebApplication.java
@@ -0,0 +1,13 @@
+package com.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCommunityWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCommunityWebApplication.class, args);
+	}
+
+}
