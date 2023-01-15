08d5595787a95bec10e8814af3f121630eecc8f1
twoosky <lhjae3@naver.com>
2023-01-15 16:54:05 +0900

    8.1 빈 생명주기 콜백 시작

---
+++ learning-spring5/core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,31 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    // [comment] ApplicationContext 인터페이스는 close 메서드를 지원하지 않는다.
+    // - ConfigurableApplicationContext 구현 객체를 사용해야 한다.
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
