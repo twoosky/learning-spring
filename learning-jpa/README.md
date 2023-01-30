d0c4fccfa9b22e43c7326e2c3ce8e1a7f3cc8a64
twoosky <lhjae3@naver.com>
2023-01-30 21:48:56 +0900

    9.6 값 타입 매핑

---
+++ learning-jpa/README.md
@@ -0,0 +1,3 @@
+## 도메인 모델
+
+<img src="https://user-images.githubusercontent.com/50009240/215477514-53b65f02-8eb1-4b76-ad32-75f88a0d9dee.png" width="630" height="350">
---
+++ learning-jpa/src/main/java/jpabook/jpashop/domain/Item.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn
+@Getter
+@Setter
+public abstract class Item extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private int stockQuantity;
+
+    @OneToMany(mappedBy = "item")
+    private List<CategoryItem> categoryItems = new ArrayList<>();
+}
