a8819d7848aed2dd54a1af4e59c63d47347d2721
twoosky <lhjae3@naver.com>
2023-03-26 23:39:02 +0900

    검증1 - Validation

---
+++ learning-spring6/validation/src/main/java/hello/itemservice/web/validation/ItemValidator.java
@@ -0,0 +1,44 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.domain.item.Item;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class ItemValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Item.class.isAssignableFrom(clazz);
+        // isAssignableFrom : class 타입 비교 함수
+        // - Item == clazz  (class 타입이 Item 인지 반환)
+        // - Item == subItem (자식 클래스인 경우에도 true)
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Item item = (Item) target;
+
+        // BindingResult는 Errors의 자식 클래스
+
+        if (!StringUtils.hasText(item.getItemName())) {
+            errors.rejectValue("itemName", "required");
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            errors.rejectValue("price", "range", new Object[]{1000, 10000000}, null);
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            errors.rejectValue("quantity", "max", new Object[]{9999}, null);
+        }
+
+        //특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                errors.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+    }
+}
