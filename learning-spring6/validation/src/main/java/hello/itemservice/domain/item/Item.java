f457779da936f683a44a832fe66c56eb47b1c28c
twoosky <lhjae3@naver.com>
2023-03-27 03:45:17 +0900

    Bean Validation - 에러 코드

--- learning-spring6/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ learning-spring6/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,28 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000")
 public class Item {
 
     private Long id;
+
+    @NotBlank(message = "공백 X")
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     public Item() {
