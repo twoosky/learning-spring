17d039347239937e80df01698c7660ec8dc5d262
twoosky <lhjae3@naver.com>
2023-03-30 01:17:34 +0900

    로그인 처리하기 - 서블릿 HTTP 세션2

---
+++ learning-spring6/login/src/main/java/hello/login/web/HomeController.java
@@ -0,0 +1,98 @@
+package hello.login.web;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
+
+//    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    /**
+     * 쿠키를 갖고있는 사용자 id만 들어오도록 required = false 설정
+     */
+//    @GetMapping("/")
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+
+        if (memberId == null) {
+            return "home";
+        }
+
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+//    @GetMapping("/")
+    public String homeLoginV2(Model model, HttpServletRequest request) {
+
+        // 세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member) sessionManager.getSession(request);
+
+        if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+//    @GetMapping("/")
+    public String homeLoginV3(Model model, HttpServletRequest request) {
+
+        // 세션 관리자에 저장된 회원 정보 조회
+        HttpSession session = request.getSession(false);
+
+        if (session == null) {
+            return "home";
+        }
+
+        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        // 세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 로그아웃 버튼이 있는 home으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3Spring(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 로그아웃 버튼이 있는 home으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+}
