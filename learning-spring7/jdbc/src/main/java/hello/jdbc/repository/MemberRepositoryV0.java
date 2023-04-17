8b3ea30cf63e120cf179de1c078cbe1f62271fbb
twoosky <lhjae3@naver.com>
2023-04-17 23:01:28 +0900

    JDBC 개발 - 등록

---
+++ learning-spring7/jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -0,0 +1,67 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.connection.DBConnectionUtil;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.*;
+
+/**
+ * JDBC - DriverManager 사용
+ */
+@Slf4j
+public class MemberRepositoryV0 {
+
+    public Member save(Member member) throws SQLException {
+        String sql = "insert into member(member_id, money) values(?, ?)";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());  // SQL 파라미터 바인딩
+            pstmt.setInt(2, member.getMoney());  // SQL 파라미터 바인딩
+            pstmt.executeUpdate();  // 쿼리 실행
+            return member;
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    // - PreparedStatement: SQL에 파라미터 바인딩 가능한 statement
+    // - Statement: SQL을 그대로 넣는 statement
+    private void close(Connection con, Statement stmt, ResultSet rs) {
+
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private Connection getConnection() {
+        return DBConnectionUtil.getConnection();
+    }
+}
