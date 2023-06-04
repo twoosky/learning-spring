480dc900f28765d25a9aac02786412cc159f1f1b
twoosky <lhjae3@naver.com>
2023-06-05 03:09:01 +0900

    동시성 문제-예제 코드

---
+++ learning-spring8/advanced/src/main/java/hello/advanced/trace/hellotrace/HelloTraceV2.java
@@ -0,0 +1,80 @@
+package hello.advanced.trace.hellotrace;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class HelloTraceV2 {
+
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<X--";
+
+    private TraceId traceIdHolder;  // traceId 동기화, 동시성 이슈 발생
+
+    public TraceStatus begin(String message) {
+        syncTraceId();
+        TraceId traceId = this.traceIdHolder;
+        Long startTimeMs = System.currentTimeMillis();
+        log.info("[" + traceId.getId() + "] " + addSpace(START_PREFIX,traceId.getLevel()) + message);
+        return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+    private void syncTraceId() {
+        if (traceIdHolder == null) {
+            traceIdHolder = new TraceId();
+        } else {
+            traceIdHolder = traceIdHolder.createNextId();
+        }
+    }
+
+    public TraceStatus beginSync(TraceId beforeTraceId, String message) {
+        TraceId nextId = beforeTraceId.createNextId();
+        Long startTimeMs = System.currentTimeMillis();
+        log.info("[" + nextId.getId() + "] " + addSpace(START_PREFIX, nextId.getLevel()) + message);
+        return new TraceStatus(nextId, startTimeMs, message);
+    }
+
+    public void end(TraceStatus status) {
+        complete(status, null);
+    }
+
+    public void exception(TraceStatus status, Exception e) {
+        complete(status, e);
+    }
+
+    private void complete(TraceStatus status, Exception e) {
+        Long stopTimeMs = System.currentTimeMillis();
+        long resultTimeMs = stopTimeMs - status.getStartTimeMs();
+        TraceId traceId = status.getTraceId();
+        if (e == null) {
+            log.info("[" + traceId.getId() + "] " + addSpace(COMPLETE_PREFIX,
+                    traceId.getLevel()) + status.getMessage() + " time=" + resultTimeMs + "ms");
+        } else {
+            log.info("[" + traceId.getId() + "] " + addSpace(EX_PREFIX,
+                    traceId.getLevel()) + status.getMessage() + " time=" + resultTimeMs + "ms" + " ex=" + e);
+        }
+
+        releaseTraceId();
+    }
+
+    private void releaseTraceId() {
+        if (traceIdHolder.isFirstLevel()) {
+            traceIdHolder = null;
+        } else {
+            traceIdHolder = traceIdHolder.createPreviousId();
+        }
+    }
+
+    private static String addSpace(String prefix, int level) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            sb.append( (i == level - 1) ? "|" + prefix : "| ");
+        }
+        return sb.toString();
+    }
+}
---
+++ learning-spring8/advanced/src/test/java/hello/advanced/trace/threadLocal/FieldServiceTest.java
@@ -0,0 +1,43 @@
+package hello.advanced.trace.threadLocal;
+
+import hello.advanced.trace.threadLocal.code.FieldService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class FieldServiceTest {
+
+    private FieldService fieldService = new FieldService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();  // Runnable 시작
+//        sleep(2000);  // fieldService.logic() 메서드 실행 끝날때 까지 대기
+        sleep(100);
+        threadB.start();
+
+        sleep(3000);  // 메인 쓰레드 종료 대기
+        log.info("main exit");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
