7ddb17f41361c427c1c447ae1b1772d235de2719
twoosky <lhjae3@naver.com>
2023-06-12 22:52:21 +0900

    인터페이스 기반 프록시와 클래스 기반 프록시

--- learning-spring8/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ learning-spring8/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,16 +2,28 @@ package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.interface_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v2_proxy.ConcreteProxyConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import({AppV1Config.class, AppV2Config.class}) // 해당 클래스를 스프링 빈으로 등록
+// @Import({AppV1Config.class, AppV2Config.class}) // 해당 클래스를 스프링 빈으로 등록
+// @Import(InterfaceProxyConfig.class)
+@Import(ConcreteProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") // 컴포넌트 스캔을 시작할 위치 지정
 public class ProxyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
+	}
+
+	@Bean
+	public LogTrace logTrace() {
+		return new ThreadLocalLogTrace();
 	}
 
 }
